87cd05b582bdba10d26940b7fea32f3be7984f32
Angel Chelaru <angel.chelaru@gmail.com>
2019-04-28 17:02:51 +0200

    Create menu for client

--- client/tftp_client.cpp
+++ client/tftp_client.cpp
@@ -9,7 +9,14 @@
 
 #define MAX_LEN 512
 #define MESSAGGIO_HELP "Sono disponibili i seguenti comandi:\r\n!help --> mostra l'elenco dei comandi disponibili\r\n!mode {txt|bin} --> imposta il modo di trasferimento dei files (testo o binario)\r\n!get filename nome_locale --> richiede al server il nome del file <filename> e lo salva localmente con il nome <nome_locale>\r\n!quit --> termina il client\r\n"
-#define COMMAND_LEN 16
+#define COMMAND_LEN 512
+
+
+
+bool startsWith(const char *str, const char *pre)
+{
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
 
 
 int main(int argc, char* argv[]){
@@ -22,14 +29,30 @@ int main(int argc, char* argv[]){
 	char quit_command[COMMAND_LEN];
 	strcpy(quit_command, "!quit\n");
 
+	char mode_command[COMMAND_LEN];
+	strcpy(mode_command, "!mode");
+
+	char get_command[COMMAND_LEN];
+	strcpy(get_command, "!get");
+
+
+
+	printf("%s", MESSAGGIO_HELP);
+
 
 	while(true) {
-		printf("%s", MESSAGGIO_HELP);
 		fgets(command, COMMAND_LEN, stdin);
 		printf("%s\n", command);
 		if(strcmp(command, help_command) == 0) {
 			printf("%s", MESSAGGIO_HELP);
 			continue;
+		}
+		if(startsWith(command, mode_command)){
+			printf("mode command");		
+			continue;
+		}
+		if(startsWith(command, get_command)){
+			printf("get command");
 		}
 
 		if(strcmp(command, quit_command) == 0){
@@ -41,3 +64,14 @@ int main(int argc, char* argv[]){
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
